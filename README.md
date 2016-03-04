6c08a349953f7a12b9c6d45519a685c6f15151d2
Arpith Siromoney <arpith@feedreader.co>
2016-03-04 18:26:41 +0530

    initial files

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# tiny-database-server
+# Tiny Database Server 
 An in-memory database server using node.js
+
+## Installation
+1. `git clone https://github.com/arpith/tiny-database-server`
+2. `cd tiny-database-server`
+3. `npm install && npm start`
+
+## Usage
+The server is accessible on http://localhost:4000/
+
+### Setting a value
+`curl http://localhost:4000/set?key=value`
+
+### Getting a value
+`curl http://localhost:4000/get?key=name`
+
+This will return the value previously stored.
---
+++ index.js
@@ -0,0 +1,49 @@
+'use strict'
+
+const http = require('http');
+const url = require('url');
+const querystring = require('querystring');
+
+const HOSTNAME = 'localhost';
+const PORT = 4000;
+
+let database = {};
+
+function addToDatabase(obj) {
+  database = Object.assign(database, obj);
+  return Promise.resolve();
+}
+
+function getFromDatabase(key) {
+  return Promise.resolve(database[key]);
+}
+
+function requestHandler(req) {
+  // Checks the route (get or set) and returns a promise to do the operation
+  // The promise returns a response object that can be serialised into JSON
+  let parsedURL = url.parse(req.url);
+  let parsedQuery = querystring.parse(parsedURL.query);
+  let response = {status: 200, body: {success: true}};
+  if (parsedURL.pathname === '/set') {
+    return addToDatabase(parsedQuery);
+  } else if (parsedURL.pathname === '/get') {
+    return getFromDatabase(parsedQuery.key);
+  } else {
+    return Promise.reject(400);
+  }
+}
+
+function requestListener(req, res) {
+  let header = { 'Content-Type': 'text/plain' };
+  requestHandler(req).then((response) => {
+    res.writeHead(200, header);
+    res.end(response);
+  }).catch((status) => {
+    res.writeHead(status, header);
+    res.end(response);
+  });
+}
+   
+http.createServer(requestListener).listen(PORT, HOSTNAME, () => {
+  console.log(`Server running at http://${HOSTNAME}:${PORT}/`);
+});
